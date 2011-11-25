413dbe6f9a45e232e44d37982208c21e7e90e37c
alex <oblovatniy@gmail.com>
2011-11-25 03:40:06 +0200

    Created page validators package + generic validator. Closes gh-1

---
+++ horus-web/src/main/java/ua/cn/stu/oop/horus/web/util/pages/validator/GenericValidator.java
@@ -0,0 +1,13 @@
+package ua.cn.stu.oop.horus.web.util.pages.validator;
+
+import ua.cn.stu.oop.horus.core.language.AvailableLocale;
+
+/**
+ *
+ * @author alex
+ */
+public abstract class GenericValidator {
+    
+    
+    public abstract String validateAndGetErrorMessageOrNull(AvailableLocale locale, Object... obj);
+}
