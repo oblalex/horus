a202831c77c50ba8a6d3c23f70081dd3f998464b
alex <oblovatniy@gmail.com>
2011-11-26 13:10:17 +0200

    Password validator's javadoc added. #2

--- horus-web/src/main/java/ua/cn/stu/oop/horus/web/util/pages/validator/PasswordValidator.java
+++ horus-web/src/main/java/ua/cn/stu/oop/horus/web/util/pages/validator/PasswordValidator.java
@@ -13,6 +13,11 @@ public class PasswordValidator extends GenericValidator {
 
     public static final int PASSWORD_LENGTH_MIN = 5;
 
+    /**
+     * @param locale sets the locale of output error message
+     * @param obj [String password, String passwordConfirm] - password with passwordConfirm to validate
+     * @return String with localized error description or null in case of successful validation
+     */
     @Override
     public String validateAndGetErrorMessageOrNull(AvailableLocale locale, Object... obj) {
 
