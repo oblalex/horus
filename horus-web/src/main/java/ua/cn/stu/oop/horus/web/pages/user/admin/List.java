c7bbd6ac00d610dff4be38b8a2072f30b12f7544
alex <oblovatniy@gmail.com>
2011-12-17 01:03:17 +0200

    Provided admin editing via ui-dialog and help of AJAX. #18.

--- horus-web/src/main/java/ua/cn/stu/oop/horus/web/pages/user/admin/List.java
+++ horus-web/src/main/java/ua/cn/stu/oop/horus/web/pages/user/admin/List.java
@@ -3,6 +3,7 @@ package ua.cn.stu.oop.horus.web.pages.user.admin;
 import java.util.Collection;
 import javax.inject.Inject;
 import org.apache.tapestry5.annotations.*;
+import org.apache.tapestry5.corelib.components.Form;
 import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.services.Request;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,14 +29,25 @@ public class List extends GenericPage{
     @Autowired
     private UserAdminService adminService;
     
+    @Persist
     @Property
     private UserAdmin item;
     
+    @Persist
+    @Property
+    private String commentVal;
+    
     private UserAdmin visitorAdmin = null;
+
+    @Component
+    private Form editForm;
     
     @Component(id = "listZone")
     private Zone listZone;
-     
+    
+    @Component(id = "editZone")
+    private Zone editZone;
+    
     public void beginRender() {
         String visitorLogin = getVisitorLogin();
         if (visitorLogin==null){
@@ -66,6 +78,21 @@ public class List extends GenericPage{
 
     public Object getListZone() {
         return request.isXHR() ? listZone.getBody() : null;
+    }
+    
+    public Object onActionFromDialogajaxlink(Long itemId) {
+        item = adminService.getEntityOrNullById(itemId);
+        commentVal = item.getComment();
+        return getEditZone();
+    }
+    
+    public void onSubmitFromEditForm() {
+        item.setComment(commentVal);
+        adminService.saveOrUpdateEntity(item);
+    }
+    
+    public Object getEditZone() {
+        return request.isXHR() ? editZone.getBody() : null;
     }
     
     @Override
