6aca242726ab3efedea46da0c4c1eb5623de6c68
alex <oblovatniy@gmail.com>
2012-01-02 14:43:04 +0200

    Added scheduler for cleaning-up directory with temporary files.

--- horus-web/src/main/java/ua/cn/stu/oop/horus/web/config/ConfigContainer.java
+++ horus-web/src/main/java/ua/cn/stu/oop/horus/web/config/ConfigContainer.java
@@ -8,31 +8,36 @@ import ua.cn.stu.oop.horus.core.io.xml.ObjectInXmlFile.*;
  * @author alex
  */
 public class ConfigContainer {
-    
+
     private static final String pathToConfig = "./horus_config.xml";
     private static final ObjectInXmlFile configInXmlFile;
     public static final Config CONFIG = new Config();
-    
-    static {        
+
+    static {
         configInXmlFile = new ObjectInXmlFile(CONFIG, pathToConfig);
         RestoringResult result = restore();
-        if (result!=RestoringResult.SUCCESS){
+        if (result != RestoringResult.SUCCESS) {
             reset();
             save();
-        }
-    }       
-    
-    public static RestoringResult restore(){
+        } else {
+            CONFIG.TMP.restartCleanUpTimer();
+        }        
+    }
+
+    public static RestoringResult restore() {
         configInXmlFile.restoreFromXmlFile();
         return configInXmlFile.getRestoringResult();
     }
-    
-    public static SavingResult save(){
+
+    public static SavingResult save() {
         configInXmlFile.saveToXmlFile();
+        if (configInXmlFile.getSavingResult().equals(SavingResult.SUCCESS)){
+            CONFIG.TMP.restartCleanUpTimer();
+        }
         return configInXmlFile.getSavingResult();
     }
-    
-    public static void reset(){
+
+    public static void reset() {
         CONFIG.reset();
     }
 }
--- horus-web/src/main/java/ua/cn/stu/oop/horus/web/config/TmpConfig.java
+++ horus-web/src/main/java/ua/cn/stu/oop/horus/web/config/TmpConfig.java
@@ -2,6 +2,8 @@ package ua.cn.stu.oop.horus.web.config;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 import ua.cn.stu.oop.horus.web.util.*;
 import ua.cn.stu.oop.horus.web.util.file.HorusFileUtils;
 
@@ -11,6 +13,7 @@ import ua.cn.stu.oop.horus.web.util.file.HorusFileUtils;
  */
 public class TmpConfig implements Resetable {
 
+    private static Timer timer;
     public String directoryPath;
     public int cleanupIntervalInSeconds;
     public int minimalFileTTLInSeconds;
@@ -22,10 +25,9 @@ public class TmpConfig implements Resetable {
                 Integer.parseInt(WebConstants.getConstant("tmp.cleanup.interval.in.seconds"));
         minimalFileTTLInSeconds =
                 Integer.parseInt(WebConstants.getConstant("tmp.min.file.ttl.in.seconds"));
-        resetOrCleanupDirectory();
     }
 
-    private void resetOrCleanupDirectory() {
+    private void createOrCleanupDirectory() {
         File f = new File(directoryPath);
         if (f.exists()) {
             cleanUp(f);
@@ -36,7 +38,7 @@ public class TmpConfig implements Resetable {
 
     private void cleanUp(File f) {
         try {
-            HorusFileUtils.cleanDirectory(f, minimalFileTTLInSeconds*1000);
+            HorusFileUtils.cleanDirectory(f, minimalFileTTLInSeconds * 1000);
         } catch (IOException ex) {
             onDirectoryCleanupIOException();
         }
@@ -47,5 +49,24 @@ public class TmpConfig implements Resetable {
 
     public String getPathToTemporary(String name) {
         return directoryPath + "/" + System.currentTimeMillis() + "-" + name;
+    }
+
+    void restartCleanUpTimer() {
+        if (timer!=null){
+            timer.cancel();
+            timer.purge();
+        }
+        
+        timer = new Timer();
+        timer.schedule(
+                new TimerTask() {
+
+                    @Override
+                    public void run() {
+                        createOrCleanupDirectory();
+                    }
+                },
+                0,
+                cleanupIntervalInSeconds * 1000);
     }
 }
