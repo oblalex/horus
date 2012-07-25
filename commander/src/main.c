534bffa19efae6ab019ec0a1a2fb24fed7f1ca2b
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-07-25 18:36:54 +0300

    Termination handler was added.

--- commander/src/main.c
+++ commander/src/main.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
+#include <signal.h>
+
 #include "game_server.h"
 
 void init();
+void setup_termination_hooks();
+void termination_handler(int);
 
 int main (int argc, char const *argv[])
 {
@@ -12,5 +16,22 @@ int main (int argc, char const *argv[])
 void init()
 {
 	term_init();
+	setup_termination_hooks();
+	
 	game_server_check_path();
+}
+
+void setup_termination_hooks()
+{
+	if (signal (SIGINT, termination_handler) == SIG_IGN)
+		signal (SIGINT, SIG_IGN);
+	if (signal (SIGHUP, termination_handler) == SIG_IGN)
+		signal (SIGHUP, SIG_IGN);
+	if (signal (SIGTERM, termination_handler) == SIG_IGN)
+		signal (SIGTERM, SIG_IGN);
+}
+
+void termination_handler(int signum)
+{
+	term_style_reset();
 }
