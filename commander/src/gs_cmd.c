8ee9918ddc8346b9d627580aec894da15144861e
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-08-04 17:14:59 +0300

    Kicking all messaging improved.

--- commander/src/gs_cmd.c
+++ commander/src/gs_cmd.c
@@ -1,11 +1,13 @@
 #include <wchar.h>
 #include <string.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "gs_cmd.h"
 #include "gs_cfg.h"
 
 #include "util/common.h"
+#include "util/print_status.h"
 #include "util/file.h"
 
 static int GS_IN_FD;
@@ -23,18 +25,28 @@ void gs_cmd_exit()
 
 void gs_cmd_chat_all(char* msg)
 {
-	char cmd[120];
+	char cmd[128];
 	sprintf(&cmd, GS_CMD_CHAT_ALL CH_NULL, msg);
 	line_wr(GS_IN_FD, cmd, strlen(cmd));
 }
 
 void gs_cmd_kick_all()
-{
+{	
+	char* msg = "Kicking all!";
+	PRINT_STATUS_NEW(msg);	
+	
+	int i;
+	for(i=0; i<3; i++)
+	{	
+		gs_cmd_chat_all(msg);
+		sleep(1);
+	}
+
 	char* channels = gs_cfg_get(GS_CFG_SEC_NET, "serverChannels");
 	char* cmd = GS_CMD_KICK_FIRST CH_NULL;
-	int i;
 
-	gs_cmd_chat_all("Kicking all");
 	for(i=0; i<atoi(channels); i++)
 		line_wr(GS_IN_FD, cmd, strlen(cmd));
+
+	PRINT_STATUS_DONE();
 }
