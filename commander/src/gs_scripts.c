c5d07f18e58111f59654c3e5ac65ced13fe8e737
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-08-05 21:29:39 +0300

    Added l10n support to server's scripts generating functions.

--- commander/src/gs_scripts.c
+++ commander/src/gs_scripts.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
+#include <wchar.h>
 #include <stdlib.h>
 
 #include "gs_scripts.h"
 #include "gs_paths.h"
 #include "gs_cmd.h"
 #include "util/print_status.h"
+#include "util/l10n.h"
 
 void gs_scripts_generate()
 {
-    PRINT_STATUS_NEW("Generating server's scripts");
+    PRINT_STATUS_NEW(tr("Generating server's scripts"));
 
     gs_scripts_generate_gc();
     gs_scripts_generate_main();
@@ -18,15 +19,10 @@ void gs_scripts_generate()
 
 void gs_scripts_generate_gc()
 {
-    PRINT_STATUS_NEW("Generating garbage collecting script");
+    PRINT_STATUS_NEW(tr("Generating garbage collecting script"));
 
     FILE* f_gc = fopen (PATH_GS_CMDF_GC, "w");
-    if (f_gc == NULL)
-    {
-        PRINT_STATUS_MSG_ERR("Unable to create file \"" GS_CMDF_GC_NAME "\"");
-        PRINT_STATUS_FAIL();
-        exit(EXIT_FAILURE);
-    }
+	check_script_file_created(f_gc, GS_CMDF_GC_NAME);
 
     fputs(GS_CMD_GC "\n", f_gc);
     fputs(GS_CMD_GC "\n", f_gc);
@@ -41,18 +37,25 @@ void gs_scripts_generate_gc()
 
 void gs_scripts_generate_main()
 {
-    PRINT_STATUS_NEW("Generating main script");
+    PRINT_STATUS_NEW(tr("Generating main script"));
 
     FILE* f_sys = fopen (PATH_GS_CMDF_MAIN, "w");
-    if (f_sys == NULL)
-    {
-        PRINT_STATUS_MSG_ERR("Unable to create file \"" GS_CMDF_MAIN_NAME "\"");
-        PRINT_STATUS_FAIL();
-        exit(EXIT_FAILURE);
-    }
+	check_script_file_created(f_sys, GS_CMDF_MAIN_NAME);
 
     fputs (GS_CMD_RUN_FILE(GS_CMDF_GC_NAME) "\n", f_sys);
     fclose (f_sys);
 
     PRINT_STATUS_DONE();
+}
+
+static void check_script_file_created(FILE* fd, char* fname)
+{
+    if (fd == NULL)
+    {
+		wchar_t msg[80];
+		swprintf(&msg, L"%s \"%s\"", tr("Unable to create file"), fname);
+        PRINT_STATUS_MSG_ERR(msg);
+        PRINT_STATUS_FAIL();
+        exit(EXIT_FAILURE);
+    }
 }
--- commander/src/gs_scripts.h
+++ commander/src/gs_scripts.h
@@ -1,8 +1,12 @@
 #ifndef GS_SCRIPTS_H
 #define GS_SCRIPTS_H
 
+#include <stdio.h>
+
 void gs_scripts_generate();
 void gs_scripts_generate_gc();
 void gs_scripts_generate_main();
+
+static void check_script_file_created(FILE* fd, char* fname);
 
 #endif // GS_SCRIPTS_H
