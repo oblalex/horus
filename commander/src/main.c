f356dddd5fcdd70ed586b92a7d2661c94b1f85c7
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-08-06 12:43:40 +0300

    Added command-line arguments parsing

--- commander/src/main.c
+++ commander/src/main.c
@@ -2,6 +2,7 @@
 
 #include "util/print_status.h"
 #include "util/l10n.h"
+#include "main_parser.h"
 #include "gs.h"
 
 void init();
@@ -9,10 +10,14 @@ void tearDown();
 
 int main (int argc, char const *argv[])
 {
-
 	init();
-	gs_init();
-	gs_run();
+	if (argc>1)
+	{
+		parseArgs(argc, argv);
+	} else {
+		gs_init();
+		gs_run();
+	}
 	tearDown();
 	exit(EXIT_SUCCESS);
 }
---
+++ commander/src/main_parser.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_PARSER_H
+#define MAIN_PARSER_H
+
+#include "util/common.h"
+
+#define ARG_VERSION 		"--version"
+#define ARG_VERSION_SHORT 	"-v"
+
+#define ARG_HELP 			"--help"
+#define ARG_HELP_SHORT 		"-h"
+
+void parseArgs(int argc, char const *argv[]);
+
+static BOOL recon_version(char const *arg);
+static void print_version();
+
+static BOOL recon_help(char const *arg);
+static void print_help(char const *exec_name);
+
+static void non_recon(char const *arg);
+
+#endif // MAIN_PARSER_H
