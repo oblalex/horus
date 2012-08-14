93cd65a2e0622b440b26fdd1c5d08c4d858a1c46
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-08-14 23:01:09 +0300

    Layout: fixed

--- js/layout.js
+++ js/layout.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 	$('head').append("<link rel='stylesheet' type='text/css' href='style/layout.css'/>");
 	
 	$('#forLayout').load("layout/layout.html", function() {
-		var forContent = $('#forContent');
-		$('#content').html(forContent.html());
+		var forContent = $('#content');
+		$('#forContent').html(forContent.html());
 		forContent.remove();		
 	});
 });
