a0de9bd9c373890a1e286558490b23932ece3349
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-09-03 01:26:28 +0300

    gs_path module ported

--- commander/src/gs_paths.c
+++ commander/src/gs_paths.c
@@ -28,9 +28,8 @@ void gs_check_path_root()
         PRINT_STATUS_MSG_ERR(tr("1. This program is located in IL-2 server's subdirectory."));
         PRINT_STATUS_MSG_ERR(tr("2. You are located at that directory currently."));
 
-		int len = 80;
-		wchar_t msg[len];
-		swprintf(msg, len, L"%s \"" GS_EXE_NAME "\".", tr("3. IL-2 server's executable file is named as"));
+		wchar_t msg[80];
+		sprintf(msg, "%s \"%s\".", tr("3. IL-2 server's executable file is named as"), GS_EXE_NAME);
 
         PRINT_STATUS_MSG_ERR((char*)&msg);
         PRINT_STATUS_FAIL();
@@ -42,27 +41,32 @@ void gs_check_path_logs()
 {
     PRINT_STATUS_NEW(tr("Checking logs path"));
 
-    struct stat st;
     char* path = PATH_GS_LOGS_DIR;
-
-    if ((stat(path, &st) != 0) && (errno == ENOENT))
+	
+#ifdef _WIN_
+	DWORD dwAttrib = GetFileAttributes(path);
+	if (!(dwAttrib != INVALID_FILE_ATTRIBUTES && 
+         (dwAttrib & FILE_ATTRIBUTE_DIRECTORY)))
+#else
+	struct stat st;
+	if ((stat(path, &st) != 0) && (errno == ENOENT))
+#endif	
     {
+		perror("ololo");
         PRINT_STATUS_MSG(tr("Creating missing directory"));
 		int mk_result;
 		
-		#ifdef _WIN_
-			mk_result = mkdir(path);
-		#else
-			mk_result = mkdir(path, S_IRWXU);
-		#endif
+	#ifdef _WIN_
+		mk_result = mkdir(path);
+	#else
+		mk_result = mkdir(path, S_IRWXU);
+	#endif
 		
         if (mk_result != 0)
         {
-			char* fail_msg = tr("Failed to create"); 
-			int len = strlen(path)+mbstowcs(NULL, fail_msg, 0)+3+1;
-            wchar_t err_msg[len];           
-			swprintf(err_msg, len, L"%s \"%s\"", fail_msg, path);
-
+			char err_msg[80];
+			sprintf(err_msg, "%s \"%s\".", tr("Failed to create"), path);
+			
             PRINT_STATUS_MSG_ERR((char*)&err_msg);
             PRINT_STATUS_FAIL();
             exit(EXIT_FAILURE);
