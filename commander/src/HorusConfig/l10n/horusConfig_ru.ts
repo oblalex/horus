821efd7db161657586762eb004777f07c0659b98
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-11-16 01:12:58 +0200

    Translations update. #40

--- commander/src/HorusConfig/l10n/horusConfig_ru.ts
+++ commander/src/HorusConfig/l10n/horusConfig_ru.ts
@@ -2,6 +2,289 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU">
 <context>
+    <name>DifficultyFlightModelPage</name>
+    <message>
+        <location filename="../difficulty_flight_model_page.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../difficulty_flight_model_page.ui" line="20"/>
+        <source>Separate engines start</source>
+        <translation>Раздельный запуск двигателей</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_flight_model_page.ui" line="27"/>
+        <source>Complex engine management</source>
+        <translation>Полное управление двигателем</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_flight_model_page.ui" line="34"/>
+        <source>Engine overheat</source>
+        <translation>Перегрев двигателя</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_flight_model_page.ui" line="41"/>
+        <source>Torque gyro effects</source>
+        <translation>Гироскопический момент</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_flight_model_page.ui" line="48"/>
+        <source>Flutter effect</source>
+        <translation>Флаттер</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_flight_model_page.ui" line="55"/>
+        <source>Stall spins</source>
+        <translation>Штопор</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_flight_model_page.ui" line="62"/>
+        <source>Blackouts &amp;&amp; redouts</source>
+        <translation>Перегрузка</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_flight_model_page.ui" line="69"/>
+        <source>G-Limits</source>
+        <translation>Динамическое ограничение перегрузки</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_flight_model_page.ui" line="76"/>
+        <source>Reliability</source>
+        <translation>Реальная надёжность двигателей</translation>
+    </message>
+</context>
+<context>
+    <name>DifficultyMapIconsPage</name>
+    <message>
+        <location filename="../difficulty_map_icons_page.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../difficulty_map_icons_page.ui" line="20"/>
+        <source>No map icons</source>
+        <translation>Отключить маркеры на карте</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_map_icons_page.ui" line="27"/>
+        <source>No player icon</source>
+        <translation>Отключить маркер игрока на карте</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_map_icons_page.ui" line="34"/>
+        <source>No fog of war icons</source>
+        <translation>Отключить маркеры разведки на карте</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_map_icons_page.ui" line="41"/>
+        <source>No minimap path</source>
+        <translation>Без маршрута на карте</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_map_icons_page.ui" line="48"/>
+        <source>No icons</source>
+        <translation>Отключить маркеры</translation>
+    </message>
+</context>
+<context>
+    <name>DifficultyPage</name>
+    <message>
+        <location filename="../difficulty_page.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../difficulty_page.ui" line="33"/>
+        <source>Difficulty</source>
+        <translation>Сложность</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_page.ui" line="102"/>
+        <source>Easy</source>
+        <translation>Легко</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_page.ui" line="109"/>
+        <source>Normal</source>
+        <translation>Нормально</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_page.ui" line="116"/>
+        <source>Real</source>
+        <translation>Как в жизни</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_page.cpp" line="41"/>
+        <source>Flight model</source>
+        <translation>Летная модель</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_page.cpp" line="49"/>
+        <source>Weapons</source>
+        <translation>Вооружение</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_page.cpp" line="57"/>
+        <source>View</source>
+        <translation>Обзор</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_page.cpp" line="65"/>
+        <source>Map &amp; icons</source>
+        <translation>Карта и маркеры</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_page.cpp" line="73"/>
+        <source>Various</source>
+        <translation>Разное</translation>
+    </message>
+</context>
+<context>
+    <name>DifficultyVarPage</name>
+    <message>
+        <location filename="../difficulty_var_page.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../difficulty_var_page.ui" line="20"/>
+        <source>Vulnerability</source>
+        <translation>Уязвимость</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_var_page.ui" line="27"/>
+        <source>Realistic pilot vulnerability</source>
+        <translation>Реалистичная уязвимость пилота</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_var_page.ui" line="34"/>
+        <source>Head shake</source>
+        <translation>Перемещения головы</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_var_page.ui" line="41"/>
+        <source>Wind &amp;&amp; turbulence</source>
+        <translation>Ветер и турбулентность</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_var_page.ui" line="48"/>
+        <source>Clouds</source>
+        <translation>Облака</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_var_page.ui" line="55"/>
+        <source>No instant success</source>
+        <translation>Выполнение задания</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_var_page.ui" line="62"/>
+        <source>Takeoff &amp;&amp; landing</source>
+        <translation>Взлёт-посадка</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_var_page.ui" line="69"/>
+        <source>Realistic landing</source>
+        <translation>Реалистичная посадка</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_var_page.ui" line="76"/>
+        <source>Realistic navigation instruments</source>
+        <translation>Реалистичная навигация</translation>
+    </message>
+</context>
+<context>
+    <name>DifficultyViewPage</name>
+    <message>
+        <location filename="../difficulty_view_page.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../difficulty_view_page.ui" line="20"/>
+        <source>No outside views</source>
+        <translation>Отключить внешние виды</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_view_page.ui" line="30"/>
+        <source>No self view</source>
+        <translation>Без собственного обзора</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_view_page.ui" line="43"/>
+        <source>No foe view</source>
+        <translation>Без обзора противника</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_view_page.ui" line="56"/>
+        <source>No friendly view</source>
+        <translation>Без обзора союзника</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_view_page.ui" line="69"/>
+        <source>No planes view</source>
+        <translation>Без обзора самолетов</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_view_page.ui" line="82"/>
+        <source>No aircraft carrier view</source>
+        <translation>Без обзора авианосцев</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_view_page.ui" line="92"/>
+        <source>Cockpit always on</source>
+        <translation>Вид только из кабины</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_view_page.ui" line="99"/>
+        <source>No speed bar</source>
+        <translation>Без индикатора скорости</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_view_page.ui" line="106"/>
+        <source>No padlock</source>
+        <translation>Отключить фиксацию взгляда</translation>
+    </message>
+</context>
+<context>
+    <name>DifficultyWeaponsPage</name>
+    <message>
+        <location filename="../difficulty_weapons_page.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../difficulty_weapons_page.ui" line="20"/>
+        <source>Realistic gunnery</source>
+        <translation>Реализм стрельбы</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_weapons_page.ui" line="27"/>
+        <source>Limited ammo</source>
+        <translation>Ограниченный боезапас</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_weapons_page.ui" line="34"/>
+        <source>Limited fuel</source>
+        <translation>Ограниченный запас топлива</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_weapons_page.ui" line="41"/>
+        <source>Bomb fuzes</source>
+        <translation>Бомбовые взрыватели</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_weapons_page.ui" line="48"/>
+        <source>Realistic torpedoing</source>
+        <translation>Реалистичное торпедирование</translation>
+    </message>
+    <message>
+        <location filename="../difficulty_weapons_page.ui" line="55"/>
+        <source>Realistic missiles variation</source>
+        <translation>Реалистичный разброс ракет</translation>
+    </message>
+</context>
+<context>
     <name>GeneralPage</name>
     <message>
         <location filename="../general_page.ui" line="14"/>
@@ -39,12 +322,12 @@
         <translation>Русский</translation>
     </message>
     <message>
-        <location filename="../general_page.cpp" line="63"/>
+        <location filename="../general_page.cpp" line="65"/>
         <source>My server</source>
         <translation>Мой сервер</translation>
     </message>
     <message>
-        <location filename="../general_page.cpp" line="64"/>
+        <location filename="../general_page.cpp" line="66"/>
         <source>No description</source>
         <translation>Без описания</translation>
     </message>
@@ -54,45 +337,45 @@
     <message>
         <location filename="../main_window.ui" line="14"/>
         <source>Horus Config</source>
-        <translation>Хорус Настройки</translation>
+        <translation>Хорус настройщик</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../settings.cpp" line="22"/>
+        <location filename="../settings.cpp" line="23"/>
         <source>File cannot be created</source>
         <translation>Файл не может быть создан</translation>
     </message>
     <message>
-        <location filename="../settings.cpp" line="23"/>
+        <location filename="../settings.cpp" line="24"/>
         <source>Horus config file cannot be created.</source>
         <translation>Файл Хорус настроек не может быть создан.</translation>
     </message>
     <message>
-        <location filename="../general_page.cpp" line="75"/>
+        <location filename="../general_page.cpp" line="77"/>
         <source>Language changed</source>
         <translation>Язык изменен</translation>
     </message>
     <message>
-        <location filename="../general_page.cpp" line="76"/>
+        <location filename="../general_page.cpp" line="78"/>
         <source>Restart application to apply new language.</source>
         <translation>Перезапустите приложение для смены языка.</translation>
     </message>
     <message>
-        <location filename="../general_page.cpp" line="77"/>
-        <location filename="../settings.cpp" line="24"/>
-        <location filename="../settings.cpp" line="37"/>
+        <location filename="../general_page.cpp" line="79"/>
+        <location filename="../settings.cpp" line="25"/>
+        <location filename="../settings.cpp" line="38"/>
         <source>Ok</source>
         <translation>Ок</translation>
     </message>
     <message>
-        <location filename="../settings.cpp" line="35"/>
+        <location filename="../settings.cpp" line="36"/>
         <source>File cannot be opened</source>
         <translation>Невозможно открыть файл</translation>
     </message>
     <message>
-        <location filename="../settings.cpp" line="36"/>
+        <location filename="../settings.cpp" line="37"/>
         <source>Server config file cannot be opened.</source>
         <translation>Файл настроек сервера не может быть открыть.</translation>
     </message>
