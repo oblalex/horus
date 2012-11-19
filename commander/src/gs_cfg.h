a9340d9daa8fb688960597f44efc2cde0b5486ab
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-11-19 11:52:28 +0200

    gs_cfg warnings fixed. #44

--- commander/src/gs_cfg.h
+++ commander/src/gs_cfg.h
@@ -6,13 +6,6 @@
 #define GS_CONSOLE_PORT "20001"
 
 void gs_cfg();
-static void gs_cfg_logging();
-static void gs_cfg_logging_chat();
-static void gs_cfg_logging_console();
-static void gs_cfg_logging_file();
-static void gs_cfg_console_connection();
-static void gs_cfg_version_checking();
-
 char* gs_cfg_get(const char* sec_name, const char* key);
 
 #endif // GS_CFG_H
