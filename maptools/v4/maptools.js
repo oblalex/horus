bc35ec613a4a486da1da0b904a87b5523ff3e8a0
Alexander Oblovatniy <oblovatniy@gmail.com>
2013-06-04 00:09:06 +0300

    v4 initialized

---
+++ maptools/v4/maptools.js
@@ -0,0 +1,53 @@
+$(function () {
+    var SCROLL_BAR_PLACE = 20
+    , map_canvas = document.getElementById("container")
+    , map_ctx = map_canvas.getContext("2d")
+    , map_canvas_wrap = $("#container-wrapper")
+    , map_img = new Image();
+
+    $(".bar").draggable({
+        containment: "parent"
+    });
+
+    $("#vbar").on("drag", function (event, ui) {
+        var ctop = (-ui.position.top * map_canvas.height / map_canvas_wrap.height());
+        map_canvas.style.top = ctop + "px"
+    });
+
+    $("#hbar").on("drag", function (event, ui) {
+        var cleft = (-ui.position.left * map_canvas.width / map_canvas_wrap.width());
+        map_canvas.style.left = cleft + "px"
+    });
+
+    map_img.onload = function (){
+        /* Init canvas and containers geometry */
+        map_canvas.width = this.width;
+        map_canvas.height = this.height;
+
+        map_canvas_wrap.width($(window).width() - SCROLL_BAR_PLACE);
+        map_canvas_wrap.height($("footer").position().top - $("#content").position().top-3 - SCROLL_BAR_PLACE);
+
+        /* Init scrollbars */
+        if(this.width > map_canvas_wrap.width()){
+            var hscroll = $("#hscroll");
+            hscroll.width(map_canvas_wrap.width()-(SCROLL_BAR_PLACE)/2);
+            hscroll.css('visibility', 'visible');
+            $("#hbar").width(hscroll.width()*hscroll.width()/this.width);
+        } else {
+            $("#hscroll").css('visibility', 'hidden');
+        }
+
+        if(this.height > map_canvas_wrap.height()){
+            var vscroll = $("#vscroll");
+            vscroll.height(map_canvas_wrap.height()-(SCROLL_BAR_PLACE)/2);
+            vscroll.css('visibility', 'visible');
+            $("#vbar").height(vscroll.height()*vscroll.height()/this.height);
+        } else {
+            $("#vscroll").css('visibility', 'hidden');
+        }
+
+        /* Draw image*/
+        map_ctx.drawImage(this, 0, 0, this.width, this.height);
+    };
+    map_img.src = "http://placehold.it/2350x1150";
+});
